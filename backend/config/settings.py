0c4b3edfe852fd27026acefdfdae33f3bf4b9ff7
SirGRasha <SirGRasha@gmail.com>
2026-08-18 01:42:17 +0330

    Role&Permissions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,15 +150,6 @@ MAILERS = {
 
 AUTH_USER_MODEL = "accounts.User"
 
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
-    ],
-}
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
